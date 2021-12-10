dbde5db894e7e6ca2519b7db599114c6403ffddb
Fraser Crichton <52700324+frasercrichton@users.noreply.github.com>
2021-12-10 19:22:14 +1300

    animation

--- src/App copy.js
+++ src/App copy.js
@@ -25,97 +25,7 @@ const MAP_STYLE = 'https://basemaps.cartocdn.com/gl/positron-nolabels-gl-style/s
 // eslint-disable-next-line import/no-webpack-loader-syntax
 mapboxgl.workerClass = require('worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker').default
 
-const defaultProps = {
-  // Frequency of the running light
-  getFrequency: { type: 'accessor', value: 1 },
-  // Speed of the running light
-  animationSpeed: { type: 'number', min: 0, value: 1 },
-  // Size of the blob
-  tailLength: { type: 'number', min: 0, value: 1 }
-}
-
-const vsDeclaration = `
-attribute float instanceFrequency;
-varying float vArcLength;
-varying float vFrequency;`
-
-const vsMain = `
-vArcLength = distance(source, target);
-vFrequency = instanceFrequency;
-`
-const fsDeclaration = `
-uniform float tailLength;
-uniform float timestamp;
-uniform float animationSpeed;
-
-varying float vArcLength;
-varying float vFrequency;`
-
-const fsColorFilter = `
-float tripDuration = vArcLength / animationSpeed;
-float flightInterval = 1.0 / vFrequency;
-float r = mod(geometry.uv.x, flightInterval);
-
-// Head of the trip (alpha = 1.0)
-float rMax = mod(fract(timestamp / tripDuration), flightInterval);
-// Tail of the trip (alpha = 0.0)
-float rMin = rMax - tailLength / vArcLength;
-// Two consecutive trips can overlap
-float alpha = (r > rMax ? 0.0 : smoothstep(rMin, rMax, r)) + smoothstep(rMin + flightInterval, rMax + flightInterval, r);
-if (alpha == 0.0) {
-  discard;
-}
-color.a *= alpha;
-`
-
-class AnimatedArcLayer extends ArcLayer {
-
-  initializeState(params) {
-    super.initializeState(params);
-
-    this.getAttributeManager().addInstanced({
-      instanceFrequency: {
-        size: 1,
-        accessor: 'getFrequency',
-        defaultValue: 1
-      },
-    });
-  }
-
-  draw(opts) {
-    console.log('drawing')
-    this.state.model.setUniforms({
-      tailLength: this.props.tailLength,
-      animationSpeed: this.props.animationSpeed,
-      timestamp: Date.now() % 86400000
-    });
-    super.draw(opts);
-
-    // By default, the needsRedraw flag is cleared at each render. We want the layer to continue
-    // refreshing.
-    this.setNeedsRedraw();
-  }
-
-  getShaders() {
-    return {
-      ...super.getShaders(),
-      inject: {
-        'vs:#decl': vsDeclaration,
-        'vs:#main-end': vsMain,
-        'fs:#decl': fsDeclaration,
-        'fs:DECKGL_FILTER_COLOR': fsColorFilter
-      }
-
-    }
-  }
-
-}
-AnimatedArcLayer.layerName = 'AnimatedArcLayer'
-AnimatedArcLayer.defaultProps = defaultProps
-const animatedId = 'arc-layer'
-//  + Date.now()
-
-function App() {
+function App () {
   const [glContext, setGLContext] = useState()
   const [hoverInfo, setHoverInfo] = useState()
   const deckRef = useRef(null)
@@ -126,6 +36,7 @@ function App() {
     latitude: -41.7409396,
     pitch: 45,
     bearing: 0,
+  
     zoom: 4.0
   })
 
@@ -135,7 +46,7 @@ function App() {
         ...viewState,
         bearing: 30,
         zoom: 5.0,
-        pitch: 90,
+        pitch: 70,
         transitionDuration: 7000,
         transitionInterpolator,
         transitionInterruption: TRANSITION_EVENTS.BREAK
@@ -188,7 +99,7 @@ function App() {
         stencil: true
       }}
     >
-      {/* <ArcLayer
+      <ArcLayer
         id='arc-layer'
         data={url}
         getSourcePosition={d => d.Transfer_From_Coordinates}
@@ -197,30 +108,15 @@ function App() {
         getTargetColor={BLUE_RGB}
         getWidth={1}
         getTilt={70}
-
-      // onHover={setHoverInfo}
-      /> */}
-      <AnimatedArcLayer
-        id={animatedId}
-        data={url}
-        getSourcePosition={d => d.Transfer_From_Coordinates}
-        getTargetPosition={d => d.Transfer_To_Coordinates}
-        getWidth={2}
-        getTilt={90}
-        getTargetColor={BLUE_RGB}
-        getSourceColor={RED_RGB}
-        getFrequency={1.0}
-        animationSpeed={0.001}
-        tailLength={1.0}
+        // onHover={setHoverInfo}
       />
-
       <StaticMap
         ref={mapRef}
         gl={glContext}
         mapboxApiAccessToken={MAP_BOX_ACCESS_TOKEN}
         onLoad={onMapLoad}
       />
-      {/* <GridLayer
+      <GridLayer
         id='grid-layer'
         data={url}
         packable
@@ -230,7 +126,7 @@ function App() {
         getPosition={d => d.Transfer_From_Coordinates}
         colorRange={[RED_RGB]}
         opacity={1.0}
-      /> */}
+      />
     </DeckGL>
   )
 }
